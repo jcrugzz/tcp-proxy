0b6c26291c8caf733f07be37d8decb4a61cf08cf
Jarrett Cruger <jcrugzz@gmail.com>
2013-10-02 02:34:08 -0400

    [fix] simple test passes! deleted unnecessary proxy-stream

--- test/simple-proxy-test.js
+++ test/simple-proxy-test.js
@@ -2,7 +2,7 @@ var net = require('net'),
     concat = require('concat-stream'),
     test = require('tap').test;
 
-var tcpProxy = require('../'),
+var tcpProxy = require('../');
 
 var proxyPort = 9000,
     serverPort = 9001;
@@ -11,37 +11,48 @@ var data = 'Hello\n',
     target = { target: { host: '127.0.0.1', port: serverPort }};
 
 test('test a simple proxy', function (t) {
-  t.plan(2);
+  t.plan(3);
   var server,
       proxyServer,
       socket;
 
   function cleanup() {
     server.close();
-    proxy.close();
+    proxyServer.close();
     socket.destroy();
 
     t.pass('cleanup success');
   }
 
-  server = net.createServer(function (socket) {
-    socket.pipe(concat(function (body) {
-      t.equals(body, data);
-      cleanup();
-    }));
+  function onError(err) {
+    t.fail(err);
+    cleanup();
+  }
+
+  server = net.createServer(function (sock) {
+    sock.on('data', function (chunk) {
+      t.equals(''+chunk, data, 'server recieved data through proxy');
+      sock.write(data);
+    });
   });
 
-  server.listen(serverPort, runProxy);
+  server.on('error', onError);
+  server.listen(serverPort, startProxy);
   function startProxy() {
-    proxyServer = tcpProxy.createServer();
-
-    proxyServer.on('error', t.fail.bind(null))
+    proxyServer = tcpProxy.createServer(target);
+    console.log('starting proxy');
+    proxyServer.on('error', onError)
     proxyServer.listen(proxyPort, runTest);
   }
 
   function runTest() {
     socket = net.connect(proxyPort);
-
+    console.log('writing data');
     socket.write(data);
+    socket.on('data', function (d) {
+      t.equals(''+d, data, 'client recieved data from server through proxy');
+      cleanup();
+    });
+
   }
 });
